74a54b3d1e78975df79f21284c79545fbab8bbdc
Kapil Krishna <kapil.krishna.yo@gmail.com>
2020-02-19 21:51:21 +0000

    Attempt #2 at Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
