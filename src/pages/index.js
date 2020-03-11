2b3b9c26005b038a82dd10e21343b28dd37a8fcc
Kapil Krishna <kapil.krishna.yo@gmail.com>
2020-03-11 10:49:28 +0000

    More margins

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,29 +11,27 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="container">
-    <div className="heroBanner">
-    <HomeBanner/>
-    </div>
-  
+      
+        <HomeBanner/>
 
-    <section className="aboutContainer">
-      <h2>About the course</h2>
-      <p>A nine-week, full-time or a 24-week, part-time bootcamp coding course is offered at Javaswitch in London for prospective coders. The part-time program offers classes during weekly evenings and Saturdays. At the end of the program, students will have a Ruby on Rails junior developer level knowledge, meaning they can prototype their ideas.</p>
-      <a className="findOutMore" href="/course">Learn more</a>
-    </section>
-    
-    <section className="testimonialContainer">
-      <h2>Hear from some of our recent graduates</h2>
-      <Testimonials/>
-    </section>
-    
-    <section className="companyContainer">
-      <h2>Companies we've worked with</h2>
-      <LogoGrid />
-    </section>
+      <section className="aboutContainer">
+        <h2>About the course</h2>
+        <p>A nine-week, full-time or a 24-week, part-time bootcamp coding course is offered at Javaswitch in London for prospective coders. The part-time program offers classes during weekly evenings and Saturdays. At the end of the program, students will have a Ruby on Rails junior developer level knowledge, meaning they can prototype their ideas.</p>
+        <a className="findOutMore" href="/course">Learn more</a>
+      </section>
+      
+      <section className="testimonialContainer">
+        <h2>Hear from some of our recent graduates</h2>
+        <Testimonials/>
+      </section>
+      
+      <section className="companyContainer">
+        <h2>Companies we've worked with</h2>
+        <LogoGrid />
+      </section>
     
   </div>
   </Layout>
 )
 
-export default IndexPage
+export default IndexPage;
